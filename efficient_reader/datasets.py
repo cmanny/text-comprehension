15f69fbd00c7d6cff40d2ac762f1feb3650497a5
Callum Mann <cm13558@my.bristol.ac.uk>
2017-02-26 23:19:38 +0000

    wrote new functions to convert dataset into tf records

--- efficient_reader/datasets.py
+++ efficient_reader/datasets.py
@@ -6,19 +6,19 @@ import tarfile
 class CBTDataSet(object):
     def __init__(self, data_dir, in_memory=False, name="dts", *args, **kwargs):
         self.in_memory = in_memory
-        self.data_dir = data_dir
+        self.top_data_dir = data_dir
         self.name = name
 
     # Function to automatically handle data
     def auto_setup(self):
         self.from_url("http://www.thespermwhale.com/jaseweston/babi/CBTest.tgz")
+        self.get_ne_data()
 
     def from_url(self, url):
-        self.inner_data = os.path.join(self.data_dir, self.name)
-        print(self.inner_data)
-        if os.exists(self.inner_data):
+        self.inner_data = os.path.join(self.top_data_dir, self.name)
+        if os.path.exists(self.inner_data):
             return
-        file_name = os.path.join(self.data_dir, self.name + ".tar.gz")
+        file_name = os.path.join(self.top_data_dir, self.name + ".tar.gz")
         try:
             headers = {
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) \
@@ -31,18 +31,99 @@ class CBTDataSet(object):
             print("Could not get the file from URL: " + url)
             raise
         with tarfile.open(name=file_name) as tf:
-            directory = os.path.join(self.data_dir, self.name)
+            directory = os.path.join(self.top_data_dir, self.name)
             os.mkdir(directory)
             tf.extractall(directory)
 
-    # I really shoudn't do this
-    def get_ne_data():
-        data_dir = os.join(self.inner_data, "CBTest", "data")
-        with open(os.join(data_dir, "cbtest_NE_test_2500ex.txt")) as test:
-            self.test = test.readlines()
+    @property
+    def data_dir(self):
+        return os.path.join(self.inner_data, "CBTest", "data")
 
-        with open(os.join(data_dir, "cbtest_NE_train.txt")) as train:
+    # I really shoudn't do this
+    def get_ne_data(self):
+        with open(os.path.join(self.data_dir, "cbtest_NE_test_2500ex.txt")) as test:
+            self.test = test.readlines()
+            print(self.test[0])
+
+        with open(os.path.join(self.data_dir, "cbtest_NE_train.txt")) as train:
             self.train = train.readlines()
 
-        with open(os.join(data_dir, "cbtest_NE_valid_2000ex.txt")) as valid:
+        with open(os.path.join(self.data_dir, "cbtest_NE_valid_2000ex.txt")) as valid:
             self.valid = valid.readlines()
+
+import os
+import pickle
+from collections import Counter
+import tensorflow as tf
+
+def get_cqa_words(cqa):
+    cqa_split = cqa.split("\n21 ")
+    context = cqa_split[0].split(" ")
+    last_line = cqa_split[1]
+    query, answer = [x.split(" ") for x in last_line.split(" ", 2)[:2]]
+    return context, query, answer
+
+def counts():
+    cache = 'counter.pickle'
+    if os.path.exists(cache):
+        with open(cache, 'r') as f:
+            return pickle.load(f)
+
+    directories = ['data/train/', 'data/valid/', 'data/test/']
+    files = [directory + file_name for directory in directories for file_name in os.listdir(directory)]
+    counter = Counter()
+    for file_name in files:
+        with open(file_name, 'r') as f:
+            file_string = f.read()
+            for cqa in file_string.split("\n\n"):
+                    if len(cqa) < 5:
+                        break
+                    context, query, answer = get_cqa_words(cqa)
+                    for token in context + query + answer:
+                        counter[token] += 1
+    with open(cache, 'w') as f:
+        pickle.dump(counter, f)
+
+    return counter
+
+def tokenize(index, word):
+    directories = ['data/train/', 'data/valid/', 'data/test/']
+    for directory in directories:
+        out_name = directory.split('/')[-2] + '.tfrecords'
+        writer = tf.python_io.TFRecordWriter(out_name)
+        files = map(lambda file_name: directory + file_name, os.listdir(directory))
+        for file_name in files:
+            with open(file_name, 'r') as f:
+                file_string = f.read()
+                for cqa in file_string.split("\n\n"):
+                    if len(cqa) < 5:
+                        break
+                    context, query, answer = get_cqa_words(cqa)
+                    context = [index[token] for token in context]
+                    query = [index[token] for token in query]
+                    answer = [index[token] for token in answer]
+                    example = tf.train.Example(
+                         features = tf.train.Features(
+                             feature = {
+                                 'document': tf.train.Feature(
+                                     int64_list=tf.train.Int64List(value=context)),
+                                 'query': tf.train.Feature(
+                                     int64_list=tf.train.Int64List(value=query)),
+                                 'answer': tf.train.Feature(
+                                     int64_list=tf.train.Int64List(value=answer))
+                                 }
+                          )
+                    )
+                    serialized = example.SerializeToString()
+                    writer.write(serialized)
+
+def main():
+    counter = counts()
+    print('num words',len(counter))
+    word, _ = zip(*counter.most_common())
+    index = {token: i for i, token in enumerate(word)}
+    tokenize(index, word)
+    print('DONE')
+
+if __name__ == "__main__":
+    main()
